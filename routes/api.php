42ab931d5314086e0d9bd6614b83d3d550d86cd3
nnivxix <adhe.hanasa@gmail.com>
2025-03-27 16:49:28 +0700

    refactor: remove deprecated movie trending and service classes, update routes

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\MovieTrendingController;
-use App\Http\Controllers\PopularMovieController;
+use App\Http\Controllers\Api\PopularMovieController;
 
 Route::get('/', function (Request $request) {
     return response()->json([
         'app'     => 'Laravel',
         'version' => app()->version(),
         'type'    => 'api'
-
     ]);
 });
 
 Route::name('api.')->group(function () {
-    Route::get('/movie/trending', MovieTrendingController::class)->name('movie.trending');
     Route::get('/movies/popular', PopularMovieController::class)->name('movies.popular');
 });
